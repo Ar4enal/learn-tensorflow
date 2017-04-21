549de2f65d8d8d07018bf4c385cb930c9b0471ba
Li Teng <930372551@qq.com>
2017-04-21 22:42:29 +0800

    上传py源码文件

---
+++ 02 Tensorflow Hello World程序/test02.py
@@ -0,0 +1,16 @@
+import tensorflow as tf 
+
+# 创建常量
+hello = tf.constant('Hello,world!')
+
+# 创建会话
+sess = tf.Session()
+
+# 执行
+result = sess.run(hello)
+
+# 关闭会话
+sess.close()
+
+# 输出结果
+print(result)
