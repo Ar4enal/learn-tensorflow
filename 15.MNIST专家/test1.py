3bdc9fb3a8b7ff7366da0a702e836ddda71c7ccc
liteng <930372551@qq.com>
2017-08-05 17:02:40 +0800

    18

--- 15.MNIST专家/test1.py
+++ 15.MNIST专家/test1.py
@@ -66,9 +66,9 @@ accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 with tf.Session() as sess:
 	sess.run(tf.global_variables_initializer())
-	for i in range(100):
+	for i in range(20000):
 		batch = mnist.train.next_batch(50)
-		if(i % 10 == 0):
+		if(i % 50 == 0):
 			train_accuracy = accuracy.eval(feed_dict = { x: batch[0], y_: batch[1], keep_prob: 1.0 })
 			print('step %d, training accuracy %g'%(i, train_accuracy))
 		train_step.run(feed_dict = { x: batch[0], y_: batch[1], keep_prob: 0.5 })
