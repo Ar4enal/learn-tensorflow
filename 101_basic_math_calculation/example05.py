8300e8b613864ba6e10a0c34d6e03ab5bc015807
liteng <930372551@qq.com>
2017-12-28 21:22:02 +0800

    add 005

---
+++ 101_basic_math_calculation/example05.py
@@ -0,0 +1,28 @@
+# 101 Basic math calculation
+
+import tensorflow as tf 
+
+a = tf.add(1, 2) # a+b
+b = tf.subtract(3, 1) # a-b
+c = tf.multiply(2, 3) # a*b
+d = tf.div(8, 4) # a/b
+e = tf.pow(2, 3) # a^b
+f = tf.sqrt(2.0) # sqrt(a)
+
+# create a session
+sess = tf.Session()
+
+# calculate
+result = sess.run([a, b, c, d, e, f])
+
+# close session
+sess.close()
+
+# output the result
+print('1+2=%s'%result[0])
+print('3-1=%s'%result[1])
+print('2*3=%s'%result[2])
+print('8/4=%s'%result[3])
+print('2^3=%s'%result[4])
+print('sqrt(2.0)=%s'%result[5])
+print('done')
