54018f160184e20a76217d31482f7eb4935378a2
liteng <930372551@qq.com>
2017-12-29 16:22:07 +0800

    add someting

--- 101_constant/example101.py
+++ 101_constant/example101.py
@@ -3,19 +3,21 @@ import tensorflow as tf
 
 a = tf.constant('Hello, world!')
 b = tf.constant(1)
-c = tf.constant(2.0)
+c = tf.constant(2.0, dtype = tf.float32)
 d = tf.constant([1, 2])
+e = tf.constant(5, dtype = tf.float32, shape = [2, 3])
 
 # create a session
 sess = tf.Session()
 
 # run the result
-result = sess.run([a, b, c, d])
+result = sess.run([a, b, c, d, e])
 
 print('a=%s'%result[0])
 print('b=%s'%result[1])
 print('c=%s'%result[2])
 print('d=%s'%result[3])
+print('e=%s'%result[4])
 print('done')
 
 # close session
--- 104_using_conditional_operators/example104.py
+++ 104_using_conditional_operators/example104.py
@@ -2,3 +2,25 @@
 
 import tensorflow as tf
 
+# constant
+a = tf.constant(1)
+b = tf.constant(2)
+c = tf.constant(3)
+
+# using conditonal operators
+f1 = lambda: a
+f2 = lambda: b
+f3 = lambda: c
+
+d = tf.case({ tf.equal(a, b): f1, tf.not_equal(a, b): f2 }, default=f3)
+
+sess = tf.Session()
+
+result = sess.run(d)
+
+print('c=%s'%result)
+print('done')
+
+sess.close()
+
+## As we all know, a equals b is wrong, and a not euqals b is right. So the result should be `c=2`.
